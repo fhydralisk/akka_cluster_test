600f9ec6861dab3a3c5cb3ac393fcfd3ea63d3ec
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2017-03-08 16:48:53 +0800

    bugfix.. hostname issue

--- src/main/scala/com/nopqzip/App.scala
+++ src/main/scala/com/nopqzip/App.scala
@@ -2,6 +2,7 @@ package com.nopqzip
 
 import akka.actor.ActorSystem
 import java.net.NetworkInterface
+import java.net.InetAddress
 
 import com.typesafe.config.ConfigFactory
 
@@ -12,9 +13,14 @@ import com.typesafe.config.ConfigFactory
 
 object App {
   def main(args: Array[String]) = {
+    val hostnameAddrs = Set("odl1.nopqzip.com", "odl2.nopqzip.com", "odl3.nopqzip.com") map {
+      e => InetAddress.getByName(e) -> e
+    } toMap
+
     val addresses = NetworkInterface.getByName("eth0").getInetAddresses()
     import scala.collection.JavaConverters._
-    val hostname = addresses.asScala.filter( _.getAddress().length == 4).next().getHostName()
+    val hostnameAddr = addresses.asScala.filter( _.getAddress().length == 4).next()
+    val hostname = hostnameAddrs.filterKeys( _ == hostnameAddr).head._2
     val config = ConfigFactory.parseString(s"""akka.remote.netty.tcp.hostname = "$hostname"""").withFallback(ConfigFactory.load())
     val system = ActorSystem("ClusterShard", config)
 
